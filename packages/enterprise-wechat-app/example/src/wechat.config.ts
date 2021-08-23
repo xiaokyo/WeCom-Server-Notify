8d66ae6cf775a574c2c97e8e6c903e2578d0850f
郑温剑 <904656778@qq.com>
2021-08-23 16:35:57 +0800

    feat(enterprise-wechat-app): 去掉wechat隐私文件
    affects: @xiaokyo/enterprise-wechat-app

--- packages/enterprise-wechat-app/example/src/wechat.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  corpid: 'ww3144c6ccf08f22ac',
-  corpsecret: '3gXb9IUZqF5o-QrYcXpYOBEz5VdcYGICsp2q_07--LI',
-  agentId: '1000002',
-
-  token: `8FoNoxxaZ-3cci-Kqh6XOYDbBD8FDL9lpXM-1vI0HODTS3-dPesjJj0hS8CDW9sVXe3D-S66fSy5SkuHfTjh9n1TBlU7OBhyukCooveeTpEEBCqbKLBBQ5aupmOWJAaR8Yl2STCm1JVl7GTOZ_dlakGh7Ds0PqXnuoutJFdM67SC1Q5xRieOQKF4lAPTi6_7y7X8k2cncN3CuHJulZhB7g`,
-}
