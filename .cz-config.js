bec6a59c3d45be52e9fda98fc41e9a5fcb3f9afa
郑温剑 <904656778@qq.com>
2021-09-08 10:15:43 +0800

    fix(root): 调整结构

---
+++ .cz-config.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+
+/**
+ * 获取scopes
+ * @returns scopes
+ */
+function getScopeEnum() {
+  const dirs = []
+  const files = fs.readdirSync('./packages')
+  files.forEach(file => {
+    const stat = fs.lstatSync('./packages/' + file)
+    if (stat.isDirectory() === true && file) dirs.push(file)
+  })
+  dirs.unshift('root')
+  return dirs
+}
+exports.getScopeEnum = getScopeEnum
+
+module.exports = {
+  types: [
+    { value: 'init', name: 'init:     初始提交' },
+    { value: 'feat', name: 'feat:     增加新功能' },
+    { value: 'fix', name: 'fix:      修复bug' },
+    { value: 'ui', name: 'ui:       更新UI' },
+    { value: 'refactor', name: 'refactor: 代码重构' },
+    { value: 'release', name: 'release:  发布' },
+    { value: 'deploy', name: 'deploy:   部署' },
+    { value: 'docs', name: 'docs:     修改文档' },
+    { value: 'test', name: 'test:     增删测试' },
+    { value: 'chore', name: 'chore:    更改配置文件' },
+    { value: 'style', name: 'style:    样式修改不影响逻辑' },
+    { value: 'revert', name: 'revert:   版本回退' },
+    { value: 'add', name: 'add:      添加依赖' },
+    { value: 'minus', name: 'minus:    版本回退' },
+    { value: 'del', name: 'del:      删除代码/文件' },
+  ],
+  scopes: getScopeEnum(),
+  messages: {
+    type: '选择更改类型:\n',
+    scope: '更改的范围:\n',
+    // 如果allowcustomscopes为true，则使用
+    // customScope: 'Denote the SCOPE of this change:',
+    subject: '简短描述:\n',
+    body: '详细描述. 使用"|"换行:\n',
+    breaking: 'Breaking Changes列表:\n',
+    footer: '关闭的issues列表. E.g.: #31, #34:\n',
+    confirmCommit: '确认提交?',
+  },
+  allowCustomScopes: false,
+  allowBreakingChanges: ['feat', 'fix'],
+}
---
+++ packages/template/README.md
@@ -0,0 +1,11 @@
+# `@xiaokyo/template`
+
+> TODO: description
+
+## Usage
+
+```
+const template = require('@xiaokyo/template');
+
+// TODO: DEMONSTRATE API
+```
